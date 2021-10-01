55e60680ad1194d900cdab367240f0ba0025d7c4
dshergilashvili <davitshergilashvili@gmail.com>
2021-10-01 17:59:35 +0400

    ConfigurationManager add

---
+++ ConfigurationManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+const string testKey = "configuration";
+
+var configuration = new ConfigurationManager();
+Console.WriteLine(configuration[testKey]);
+
+configuration.AddInMemoryCollection(new Dictionary<string, string>()
+{
+    { testKey, "configuration" }
+});
+
+Console.WriteLine(configuration[testKey]);
+Console.ReadLine();
